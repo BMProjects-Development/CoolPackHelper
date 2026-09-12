e923edc955e637851326a211ca79220d988090f3
BarMaxx <98416932+BarMaxx@users.noreply.github.com>
2026-09-12 18:35:05 +0300

    init

---
+++ src/main/kotlin/org/bmp/cph/block/ModBlocks.kt
@@ -0,0 +1,18 @@
+package org.bmp.cph.block
+
+import org.bmp.cph.Cph
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.neoforged.neoforge.registries.DeferredRegister
+
+// THIS LINE IS REQUIRED FOR USING PROPERTY DELEGATES
+import thedarkcolour.kotlinforforge.neoforge.forge.getValue
+
+object ModBlocks {
+    val REGISTRY = DeferredRegister.createBlocks(Cph.ID)
+
+    // If you get an "overload resolution ambiguity" error, include the arrow at the start of the closure.
+    val EXAMPLE_BLOCK by REGISTRY.register("example_block") { ->
+        Block(BlockBehaviour.Properties.of().lightLevel { 15 }.strength(3.0f))
+    }
+}
